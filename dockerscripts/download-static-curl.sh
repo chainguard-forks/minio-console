0004b3d421b82006c0e1bf91b026cb5e5523a474
Kristof Bach <crys@crys.hu>
2026-01-26 20:33:24 +0100

    fix: use previous static-curl release, as latest doesn't have aarch64

--- dockerscripts/download-static-curl.sh
+++ dockerscripts/download-static-curl.sh
@@ -2,7 +2,7 @@
 
 function download_arch_specific_executable {
 	curl -f -L -s -q \
-		https://github.com/moparisthebest/static-curl/releases/latest/download/curl-$1 \
+		https://github.com/moparisthebest/static-curl/releases/v8.11.0/download/curl-$1 \
 		-o /go/bin/curl || exit 1
 	chmod +x /go/bin/curl
 }
